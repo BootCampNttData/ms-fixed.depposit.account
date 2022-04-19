2b65c4078f0dd669d813561c2369f47826711b86
Juan Jose Alejo <juanjose.alejofalcon@nttdata.com>
2022-04-19 11:10:20 -0500

    Microservicio para Depositos a plazo fijo

---
+++ src/test/java/com/bootcamp/fixeddeppossitaccount/FixedDeppossitAccountApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcamp.fixeddeppossitaccount;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FixedDeppossitAccountApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
