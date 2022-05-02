ae5353a87665bfb85ea4e587923b376441e54a57
Juan Jose Alejo <juanjose.alejofalcon@nttdata.com>
2022-05-01 19:23:39 -0500

    Update Microservicios

---
+++ src/main/java/com/bootcamp/fixeddeppossitaccount/util/Constants.java
@@ -0,0 +1,11 @@
+package com.bootcamp.fixeddeppossitaccount.util;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+
+@RequiredArgsConstructor
+public class Constants {
+    @Value("${constants.url.server}")
+    public static String gwServer;
+
+}
